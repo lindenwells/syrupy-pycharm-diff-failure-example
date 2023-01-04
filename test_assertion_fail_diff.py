18a1ef52d5a5653e0794679965853a9aa77f120e
Linden Wells <62700647+lindenwells@users.noreply.github.com>
2023-01-04 15:46:24 +1000

    Shrink the example size

--- test_assertion_fail_diff.py
+++ test_assertion_fail_diff.py
@@ -1,5 +1,7 @@
 import syrupy
 
+
+# I was trying to think of a complex nested object and thought of the lyrics in Tyler the Creator's 'Foreword'
 class View:
     direction: str = ""
 
@@ -30,6 +32,6 @@ class Mansion:
         self.rooms = rooms
 
 
-def test_failing_test(snapshot: syrupy.SnapshotAssertion):
+def test_minimal_failing_example(snapshot: syrupy.SnapshotAssertion):
     """Supposed to fail as I'm trying to recreate the 'Click to see difference' text in the PyCharm test runner."""
-    assert Mansion(rooms=[Room(name="kitchen", windows=[Window(views={View("NE"), View("E")})])]) == snapshot
+    assert Mansion(rooms=[Room(name="dungeon", windows=[])]) == snapshot
